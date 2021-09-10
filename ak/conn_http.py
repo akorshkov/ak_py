3b3412bc5ab3209ada9d4c054b369953471fdf71
A Korshkov <akorshkov@gmail.com>
2021-09-10 17:57:14 +0300

    major redesign conn_http. work in progress

--- ak/conn_http.py
+++ ak/conn_http.py
@@ -18,17 +18,44 @@ logger = logging.getLogger(__name__)
 
 
 #########################
-# http connection
+# http connections
 #
 # Send http requests to specified address and process responses
+#
+# Different types of connections (f.e. connection which sends requests with
+# basic authentication) are implemented as wrappers: such connection adds
+# _RequestAdapter object to request processing sequence and delegates
+# actual call to the http connection it wraps.
+#
+# Finally the request is processed by _HttpConnImpl object.
 
-class HttpConn:
-    """Allowes to make http(s) requests."""
 
-    def __init__(self, address,
-                 *, _send_request_ids=True, adapters=None):
+class _RequestAdapter:
+    # base class for adapters, which can be used when processing requests
+    # by HttpConn objects
+    def process_request(self, request):
+        """pre-process http request"""
+        pass
+
+    def process_response(self, return_value, is_error):
+        """process value returned by http request"""
+        # pylint: disable=unused-argument
+        return return_value
+
+    def mk_descr(self):
+        """Prepare adapter's description to be included into connection description"""
+        return None
+
+
+class _HttpConnImpl:
+    # actually makes http requests and process results
+    #
+    # it is supposed to be used by 'public' classes (HttpConn, BAuthConn, etc.)
+
+    def __init__(self, address, _send_request_ids=True):
         self.address = address
-        self.adapters = adapters or []
+        self.adapters = []  # dummy value. Child connections expect this property
+        self.conn_impl = self
 
         self._reqid_generator_guard = threading.Lock()
         self._reqid_connection_part = "".join(
@@ -38,34 +65,11 @@ class HttpConn:
         is_https = address.lower().startswith("https://")
         self.opener = self._make_opener(is_https)
 
-        self.descr = None
-
     def __str__(self):
-        # return f"Connection to {self.address}"
-        if self.descr is None:
-            self._prepare_self_descr()
-        return self.descr
+        return f"Connection to {self.address}"
 
     def __repr__(self):
         return str(self)
-
-    def _prepare_self_descr(self):
-        # create description of self. It includes descriptions of adapters
-        def _parts():
-            # prepare parts of description
-            yield f"Connection to '{self.address}'"
-            for adapter in self.adapters:
-                yield adapter.mk_descr()
-
-        self.descr = " ".join(part for part in _parts() if part is not None)
-
-    def add_adapter(self, adapter):
-        """Add adapter to self.
-
-        Adapter is an object that can process request and returned value.
-        """
-        self.adapters.append(adapter)
-        self.descr = None  # to be evaluated when necessary
 
     @staticmethod
     def _make_opener(is_https, if_http_debug=False):
@@ -84,22 +88,14 @@ class HttpConn:
             http_handler = urllib.request.HTTPHandler(debuglevel=debuglevel)
         return urllib.request.build_opener(http_handler)
 
-    def __call__(self, path, method=None, *,
-                 params=None, data=None, headers=None, raw_response=False):
-        """Make specified https(s) request.
+    def do_request(self, adapters, path, method=None,
+                    params=None, data=None, headers=None, raw_response=False):
+        """Method which makes the request.
 
-        Arguments:
-        - path: request path
-        - method: request method. By default is "GET" or "POST" (if 'data' provided)
-        - params: dictionary of request parameters
-        - data: data for request. Can be bytes, string or any python object, which
-            can be dumped to json.
-        - headers: dict, explicit headers for request
-        - raw_response: specifies how response is handled.
-            False - (default) method expects that response contains a valid
-               json (or no data) and returns decoded json (or empty str). Exception
-               is thrown if request was not successful.
-            True - urllib Response object is returned "as-is".
+        To be used by the owner connection object. (owner just keeps list of
+        adapters and calls this method with these adapters)
+
+        Descriptions of other arguments are same as in _HttpConnBase.__call__()
         """
 
         # 1. params
@@ -120,7 +116,7 @@ class HttpConn:
         if not method:
             method = 'POST' if data else 'GET'
         else:
-            method = method.upper()
+            method = str(method).upper()
 
         # 4. data
         if data is None:
@@ -143,7 +139,7 @@ class HttpConn:
             headers=req_headers)
 
         # custom pre-process request. (f.e. add auth headers)
-        for adapter in self.adapters:
+        for adapter in adapters:
             adapter.process_request(request)
 
         # log request.
@@ -173,7 +169,7 @@ class HttpConn:
             ret_val = response
 
         # process return value
-        for adapter in self.adapters[::-1]:
+        for adapter in adapters[::-1]:
             ret_val = adapter.process_response(ret_val, is_error)
 
         return ret_val
@@ -219,89 +215,177 @@ class HttpConn:
             response._method, req_path, response.code, headers_descr, response.data)
 
 
-class _HttpConnAdapter:
-    # base class for adapters, which can be used when processing requests
-    # by HttpConn objects
-    def process_request(self, request):
-        """pre-process http request"""
-        pass
+class _HttpConnBase:
+    # Base class for http connection objects.
+    #
+    # Derived classes have only to implement apropriate request adapters
 
-    def process_response(self, return_value, is_error):
-        """process value returned by http request"""
-        # pylint: disable=unused-argument
-        return return_value
+    def __init__(self, adapters, conn_data):
+        # Arguments:
+        # - adapters: list of _RequestAdapter objects or a single such object
+        # - conn_data: either another _HttpConnBase object to wrap, or arguments
+        #              for _HttpConnImpl
 
-    def mk_descr(self):
-        """Prepare adapter's description to be included into connection description"""
-        return None
+        if not isinstance(adapters, (list, tuple)):
+            # 'adapters' is not a list, but a single adapter. Make it a list
+            adapters = [adapters, ]
+
+        if hasattr(conn_data, 'conn_impl') and callable(conn_data):
+            # conn_data is another connection object, self will wrap it
+            parent_conn = conn_data
+        elif isinstance(conn_data, str):
+            # conn_data is an address
+            parent_conn = _HttpConnImpl(conn_data)
+        elif isinstance(conn_data, (list, tuple)):
+            # conn_data is a list of arguments for connection
+            parent_conn = _HttpConnImpl(*conn_data)
+        elif isinstance(conn_data, dict):
+            # conn_data is a dict of arguments for connection
+            parent_conn = _HttpConnImpl(**conn_data)
+        else:
+            assert False, (
+                f"Unexpected value of 'conn_data' arg (type {type(conn_data)}): "
+                f"{conn_data}")
+
+        self.parent_conn = parent_conn
+        self.conn_impl = parent_conn.conn_impl
+
+        self.own_adapters = adapters
+        # prepare final list of adapters(including own adapters and those
+        # used by parent connection)
+        self.adapters = self.own_adapters + self.parent_conn.adapters
+
+        self.descr = None  # to be evaluated on demand
+
+    def __str__(self):
+        if self.descr is None:
+            self._prepare_self_descr()
+        return self.descr
+
+    def __repr__(self):
+        return str(self)
+
+    def _prepare_self_descr(self):
+        # create description of self. It includes descriptions of adapters
+        def _parts():
+            # prepare parts of description
+            for adapter in self.own_adapters[::-1]:
+                yield adapter.mk_descr()
+            yield str(self.parent_conn)
+
+        self.descr = " ".join(part for part in _parts() if part is not None)
+
+    def add_adapter(self, adapter):
+        """Add adapter to self.
+
+        Adapter is an object that can process request and returned value.
+        """
+        self.adapters.append(adapter)
+        self.descr = None  # to be re-evaluated by demand
+
+    def __call__(self, path, method=None, *,
+                 params=None, data=None, headers=None, raw_response=False):
+        """Make specified https(s) request.
+
+        Arguments:
+        - path: request path
+        - method: request method. By default is "GET" or "POST" (if 'data' provided)
+        - params: dictionary of request parameters
+        - data: data for request. Can be bytes, string or any python object, which
+            can be dumped to json.
+        - headers: dict, explicit headers for request
+        - raw_response: specifies how response is handled.
+            False - (default) method expects that response contains a valid
+               json (or no data) and returns decoded json (or empty str). Exception
+               is thrown if request was not successful.
+            True - urllib Response object is returned "as-is".
+        """
+        # small shortcat: instead of calling self.parent_conn call the final
+        # element of the chain immediately
+        return self.conn_impl.do_request(
+            self.adapters, path, method, params, data, headers, raw_response)
 
 
-class BAuthAdapter(_HttpConnAdapter):
-    """Add Basic Authorization header to http(s) request."""
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
-        self.bauth_header = b"Basic " + base64.b64encode(
-            f"{login}:{password}".encode('utf-8'))
-
-    def process_request(self, request):
-        """Pre-process http(s) request: add auth header"""
-        assert not request.has_header('Authorization')
-        request.add_header('Authorization', self.bauth_header)
-
-    def mk_descr(self):
-        """Prepare adapter's description to be included into connection description"""
-        return f"with bauth by '{self.login}'"
+class HttpConn(_HttpConnBase):
+    """Send plain http requests"""
+    def __init__(self, conn_data):
+        super().__init__([], conn_data)
 
 
-class ClientAuthAdapter(_HttpConnAdapter):
-    """Add Client authorization to http(s) request."""
-    def __init__(self, client_name, client_id, client_secret):
-        self.client_name = client_name
-        self.client_id = client_id
-        self.client_secret = client_secret
+class BAuthConn(_HttpConnBase):
+    """Send http requests with basic authentication"""
 
-        self.bauth_header = b"Basic " + base64.b64encode(
-            f"{client_id}:{client_secret}".encode('utf-8'))
+    class Adapter(_RequestAdapter):
+        """Add Basic Authorization header to http(s) request."""
+        def __init__(self, login, password):
+            self.login = login
+            self.password = password
+            self.bauth_header = b"Basic " + base64.b64encode(
+                f"{login}:{password}".encode('utf-8'))
 
-    def process_request(self, request):
-        """Pre-process http(s) request: add auth header"""
-        assert not request.has_header('Authorization')
-        request.add_header('Authorization', self.bauth_header)
+        def process_request(self, request):
+            """Pre-process http(s) request: add auth header"""
+            assert not request.has_header('Authorization')
+            request.add_header('Authorization', self.bauth_header)
 
-    def mk_descr(self):
-        """Prepare adapter's description to be included into connection description"""
-        return f"with bauth by client '{self.client_name}'"
+        def mk_descr(self):
+            """Prepare part for connection description"""
+            return f"BAuth by '{self.login}'"
 
-
-class TokenAuthAdapter(_HttpConnAdapter):
-    """Add token authorization to http(s) request."""
-    def __init__(self, token, token_descr=None):
-        self.token_descr = token_descr
-        self.header = f"Bearer {token}"
-
-    def process_request(self, request):
-        """Pre-process http(s) request: add auth header"""
-        assert not request.has_header('Authorization')
-        request.add_header('Authorization', self.header)
-
-    def mk_descr(self):
-        """Prepare adapter's description to be included into connection description"""
-        descr = "with auth by token"
-        if self.token_descr is not None:
-            descr += f" '{self.token_descr}'"
-        return descr
+    def __init__(self, conn_data, login, password):
+        super().__init__(self.Adapter(login, password), conn_data)
 
 
-def bauth_conn(address, login, password, **kwargs):
-    """Create HttpConn with basic authorization."""
-    adapters = kwargs.pop('adapters', [])
-    bauth_adapter = BAuthAdapter(login, password)
-    adapters.append(bauth_adapter)
+class ClientAuthConn(_HttpConnBase):
+    """Send http requests with client basic authentication"""
 
-    conn = HttpConn(
-        address,
-        adapters=adapters,
-        **kwargs)
+    class Adapter(_RequestAdapter):
+        """Add Client authorization to http(s) request."""
+        def __init__(self, client_name, client_id, client_secret):
+            self.client_name = client_name
+            self.client_id = client_id
+            self.client_secret = client_secret
 
-    return conn
+            self.bauth_header = b"Basic " + base64.b64encode(
+                f"{client_id}:{client_secret}".encode('utf-8'))
+
+        def process_request(self, request):
+            """Pre-process http(s) request: add auth header"""
+            assert not request.has_header('Authorization')
+            request.add_header('Authorization', self.bauth_header)
+
+        def mk_descr(self):
+            """Prepare part for connection description"""
+            return f"with BAuth by client '{self.client_name}'"
+
+    def __init__(self, conn_data, client_name, client_id, client_secret):
+        super().__init__(
+            self.Adapter(client_name, client_id, client_secret),
+            conn_data)
+
+
+class TokenAuthConn(_HttpConnBase):
+    """Send http requests with token authentication"""
+
+    class Adapter(_RequestAdapter):
+        """Add token authorization to http(s) request."""
+        def __init__(self, token, token_descr=None):
+            self.token_descr = token_descr
+            self.header = f"Bearer {token}"
+
+        def process_request(self, request):
+            """Pre-process http(s) request: add auth header"""
+            assert not request.has_header('Authorization')
+            request.add_header('Authorization', self.header)
+
+        def mk_descr(self):
+            """Prepare part for connection description"""
+            descr = "with auth by token"
+            if self.token_descr is not None:
+                descr += f" '{self.token_descr}'"
+            return descr
+
+    def __init__(self, conn_data, token, token_descr=None):
+        super().__init__(
+            self.Adapter(token, token_descr),
+            conn_data)
