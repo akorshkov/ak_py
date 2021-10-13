94c548feabe0b0146289a3d0ada37d3db8a826d7
A Korshkov <akorshkov@gmail.com>
2021-10-13 11:05:47 +0300

    remove support of components from sql methods

--- ak/mcaller_sql.py
+++ ak/mcaller_sql.py
@@ -14,7 +14,7 @@ class MCallerMetaSqlMethod:
     _MAKE_BM_NOTES_METHOD = '_make_bm_notes_sql'
 
     def __init__(self, component):
-        self.component = component
+        _ = component
 
 
 def method_sql(component=None):
@@ -39,115 +39,44 @@ def method_sql(component=None):
 
 
 class MCallerSql(MCaller):
-    """Base class for "sql method callers".
+    """Base class for "sql method callers"."""
+    def __init__(self, db_conn,
+                 db_connector=None, connector_args=None, connector_kwargs=None):
+        """Create sql methods caller.
 
-    Derived class should look like:
-    class MyCaller(MCallerSql):
+        Arguments:
+        - db_conn: db connection object
+        - db_connector, connector_args, connector_kwargs: optional values, which
+        can be used to re-create db_conn.
+        """
+        if db_connector is None:
+            assert connector_args is None and connector_kwargs is None
+        else:
+            if connector_args is None:
+                connector_args = []
+            if connector_kwargs is None:
+                connector_kwargs = {}
 
-        _SQL_MTD_1 = SqlMethod(....)
+        self.db_conn = db_conn
+        self.db_connector = db_connector
+        self.connector_args = connector_args
+        self.connector_kwargs = connector_kwargs
+        if self.db_conn is None:
+            self.reconnect()
 
-        def __init__(self, ...):
-            self.sql_dbs_addrs = {
-                # component_name -> db-specific info required to connect db
-            }
-
-        def _make_sql_conn(self, component_name, conn_info):
-            # conn_info is self.sql_dbs_addrs[component_name]
-            # create real db connection and return it
-            return conn
-
-        @method_sql('componentX')
-        def get_something_from_db(self, some_args):
-            db_conn = self.get_sql_conn()
-            return self.SQL_MTD_1.one_or_none(db_conn, some_args)
-
-    In case component names are not used in 'method_sql' decorators
-    a single self.sql_db_addr can be used instead of a self.sql_dbs_addrs dict.
-    """
+    def reconnect(self):
+        """Reconnect to database"""
+        assert self.db_connector is not None
+        self.db_conn = self.db_connector(
+            *self.connector_args, **self.connector_kwargs)
 
     def get_sql_conn(self):
-        """Returns sql connection to be used in current sql wrapper method.
-
-        Returned sql connection depends on 'method_sql' metadata of caller.
-        So this method uses some 'inspect' magic to find this metadata.
-        """
-        # pylint: disable=no-member
-        method_meta = self.get_mcaller_meta()  # 'inspect' magic is in there
-
-        assert isinstance(method_meta, MCallerMetaSqlMethod), (
-            "Method 'get_sql_conn' can only be called from sql request wrappers"
-        )
-
-        if not hasattr(self, '_mc_sql_conns'):
-            setattr(self, '_mc_sql_conns', {})
-
-        if method_meta.component not in self._mc_sql_conns:
-            # get the connection info
-            conn_map_is_used = hasattr(self, 'sql_dbs_addrs')
-            single_conn_is_used = hasattr(self, 'sql_db_addr')
-            if not(conn_map_is_used or single_conn_is_used):
-                raise ValueError(
-                    f"obj of class {type(self)} has both 'sql_db_addr' "
-                    f"should have database connection information stored in "
-                    f"either 'sql_db_addr' attribute or in 'sql_dbs_addrs' "
-                    f"dictionary (in case sql methods have 'component' property.)")
-            if conn_map_is_used and single_conn_is_used:
-                raise ValueError(
-                    f"obj of class {type(self)} has both 'sql_db_addr' "
-                    f"'sql_dbs_addrs' attributes. If 'sql_dbs_addrs' is used "
-                    f"the information about db connection for methods w/o "
-                    f"specified component should be stored in "
-                    f"sql_dbs_addrs[None].")
-            if conn_map_is_used:
-                if method_meta.component not in self.sql_dbs_addrs:
-                    raise ValueError(
-                        f"obj of class {type(self)}: db address of component "
-                        f"'{method_meta.component}' is not found in "
-                        f"'sql_dbs_addrs' dictionary.")
-                db_addr = self.sql_dbs_addrs[method_meta.component]
-            else:
-                if method_meta.component is not None:
-                    raise ValueError(
-                        f"obj of class {type(self)} has sql method with "
-                        f"component '{method_meta.component}', but does not "
-                        f"have 'sql_dbs_addrs' attribute")
-                db_addr = self.sql_db_addr
-
-            self._mc_sql_conns[method_meta.component] = self._make_sql_conn(
-                method_meta.component, db_addr)
-
-        return self._mc_sql_conns[method_meta.component]
-
-    def _make_sql_conn(self, _component_name, db_addr):
-        # create actual db connection for specified component
-        # (db_addr is db address information fetched from self.sql_dbs_addrs
-        # or self.sql_db_addr)
-        #
-        # Default implementation assumes that db_addr is already an actual
-        # db connection. Override in derived class if required
-        return db_addr
+        """Returns sql connection to be used in current sql wrapper method."""
+        return self.db_conn
 
     def _make_bm_notes_sql(self, bound_method, palette) -> BoundMethodNotes:
         # create BoundMethodNotes for bound sql-request method (method
         # decorated with 'method_sql')
         assert hasattr(bound_method, '_mcaller_meta')
-        method_meta = bound_method._mcaller_meta
-        for attr in ['component']:
-            # '_make_bm_notes_sql' must have been specified in method_meta,
-            # so method_meta must have these attributes
-            assert hasattr(method_meta, attr)
 
-        if hasattr(self, 'sql_dbs_addrs'):
-            if method_meta.component in self.sql_dbs_addrs:
-                return BoundMethodNotes(True, "", None)
-        elif hasattr(self, 'sql_db_addr'):
-            if method_meta.component is None:
-                return BoundMethodNotes(True, "", None)
-
-        # connection to specified component not available
-        if method_meta.component is not None:
-            return BoundMethodNotes(
-                False, '<n/a>',
-                f"db component '{method_meta.component}' is not configured")
-        return BoundMethodNotes(
-            False, '<n/a>', "db connection is not configured")
+        return BoundMethodNotes(True, "", None)
--- tests/test_mcaller_sql.py
+++ tests/test_mcaller_sql.py
@@ -31,22 +31,6 @@ class TestMCallerSQL(unittest.TestCase):
             db_conn = self.get_sql_conn()
             return self._MTD_SQL_GET_ACCOUNT.one(db_conn, account_id)
 
-    class MethodsCollection2(MCallerSql):
-        """Another collection of sql requests wrappers."""
-
-        _MTD_SQL_GET_DOC = SqlMethod(
-            "SELECT id, description FROM documents WHERE id = ?;",
-            ['document_id',],
-            'document',
-            ['doc_id', 'descr'],
-        )
-
-        @method_sql('docs_component')
-        def get_document(self, doc_id):
-            """Get account by id."""
-            db_conn = self.get_sql_conn()
-            return self._MTD_SQL_GET_DOC.one(db_conn, doc_id)
-
     def _make_sample_db_accts(self):
         # create sample sqlite3 db for tests
         db = sqlite3.connect(":memory:")
@@ -57,21 +41,6 @@ class TestMCallerSQL(unittest.TestCase):
         cur.executemany(
             "INSERT INTO accounts (id, name) VALUES (?, ?)",
             [(1, "MI6"), (2, "MathLab")],
-        )
-        db.commit()
-
-        return db
-
-    def _make_sample_db_docs(self):
-        # create sample sqlite3 db for tests
-        db = sqlite3.connect(":memory:")
-        cur = db.cursor()
-        cur.execute(
-            "CREATE TABLE documents "
-            "(id PRIMARY KEY, description TEXT) ")
-        cur.executemany(
-            "INSERT INTO documents (id, description) VALUES (?, ?)",
-            [(10, "python tutorial"), (20, "c++ book")],
         )
         db.commit()
 
@@ -89,8 +58,7 @@ class TestMCallerSQL(unittest.TestCase):
         # 1. Check behavior of class with single connection
         class MySqlCallerSingleConn(self.MethodsCollection1):
             """Methods of this class wrap sql requests."""
-            def __init__(self, db):
-                self.sql_db_addr = db
+            pass
 
         h_text_class = h(MySqlCallerSingleConn)
         self.assertIn("get_account", h_text_class)
@@ -104,69 +72,3 @@ class TestMCallerSQL(unittest.TestCase):
 
         h_text_obj = h(my_sql_caller)
         self.assertIn("get_account", h_text_obj)
-
-        # 2. Check behavior of a similar class with map of connections
-        class MySqlCallerMapConns(self.MethodsCollection1):
-            """Methods of this class wrap sql requests."""
-            def __init__(self, db):
-                self.sql_dbs_addrs = {None: db}
-
-        h_text_class = h(MySqlCallerMapConns)
-        self.assertIn("get_account", h_text_class)
-
-        my_sql_caller = MySqlCallerMapConns(db)
-
-        account = my_sql_caller.get_account(1).r
-        self.assertEqual("MI6", account.name)
-        account = my_sql_caller.get_account_r(1)
-        self.assertEqual("MI6", account.name)
-
-        h_text_obj = h(my_sql_caller)
-        self.assertIn("get_account", h_text_obj)
-
-    def test_sql_caller_not_available_method(self):
-        """Check sql caller with not some components not configured."""
-        # create and populate database
-        db_accts = self._make_sample_db_accts()
-        db_docs = self._make_sample_db_docs()
-
-        h = HCommand()._make_help_text
-
-        class MySqlCaller(self.MethodsCollection1, self.MethodsCollection2):
-            """Methods of this class wrap sql requests."""
-            def __init__(self, dbs_addrs):
-                self.sql_dbs_addrs = dbs_addrs
-
-        h_text_class = h(MySqlCaller)
-        self.assertIn("get_account", h_text_class)
-        self.assertIn("get_document", h_text_class)
-
-        # check behavior of caller with configured connection to accounts db only
-        caller_accts = MySqlCaller({None: db_accts})
-
-        h_text_obj = h(caller_accts)
-        self.assertIn("get_account", h_text_obj)
-        self.assertNotIn(
-            "get_document", h_text_obj,
-            "expected n/a because conn to docs db not specified")
-
-        account = caller_accts.get_account(1).r
-        self.assertEqual("MI6", account.name)
-
-        with self.assertRaises(ValueError) as err:
-            caller_accts.get_document(7)
-        err_msg = str(err.exception)
-        self.assertIn('docs_component', err_msg)
-
-        # check behavior of caller with configured connection to all required dbs
-        caller = MySqlCaller({None: db_accts, 'docs_component': db_docs})
-
-        h_text_obj = h(caller)
-        self.assertIn("get_account", h_text_obj)
-        self.assertIn("get_document", h_text_obj)
-
-        account = caller.get_account(1).r
-        self.assertEqual("MI6", account.name)
-
-        doc = caller.get_document(20).r
-        self.assertEqual("c++ book", doc.descr)
